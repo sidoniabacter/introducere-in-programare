a3e5a3be30966138f252befa0f80663e265da125
Bacter Sidonia <sidoniabacter17@gmail.com>
2026-03-24 09:36:07 +0200

    adding modifications

--- Student.java
+++ Student.java
@@ -20,18 +20,45 @@ public String toString(){
     return "Student: "+name+"{"+yearOfStudies+"}";
 }
 
+public static void setYearOfStudies(Student student, int yearOfStudies){
+    student.yearOfStudies = yearOfStudies;
+}
+
+public static boolean parameterManagement(int value){
+    value=10;
+    System.out.println("value inside method "+value);
+    return true;
+}
+
+public boolean equals(Student student){
+    return this.name.equals(student.name) && this.yearOfStudies ==student.yearOfStudies;
+}
 
      public static void main(String[] args) throws Exception {
-        Student student = new Student("Teo");
-        System.out.println("Hello, "+student);
+        Student s1=new Student("John");
+        Student s2=new Student("John");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
 
-// suma a n numere naturale
-Scanner scan =new Scanner(System.in);
-int n=scan.nextInt();
- int suma = 0;
-     for(int i=0;i<=n;i++){
-        suma=suma +i;
-     }
-     System.out.println("suma primelor "+n+" numere este "+suma);
-    }
+        // System.out.println(s1);
+        // setYearOfStudies(s1,3);
+        // System.out.println(s1);
+        // int value =5;
+        // System.out.println("Value before method call "+value);
+        // parameterManagement(value);
+        // System.out.println("Value after method call "+value);
+        // System.out.println(parameterManagement(value));
+        // Student student = new Student("Teo");
+        // System.out.println("Hello, "+student);
+
+// // suma a n numere naturale
+// Scanner scan =new Scanner(System.in);
+// int n=scan.nextInt();
+//  int suma = 0;
+//      for(int i=0;i<=n;i++){
+//         suma=suma +i;
+//      }
+//      System.out.println("suma primelor "+n+" numere este "+suma);
+   
+}
 }
