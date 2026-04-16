474c373d06bfe6c03563c3819fcc3053f1aecd1c
Bacter Sidonia <sidoniabacter17@gmail.com>
2026-04-16 20:12:32 +0300

    some modifications remove some spaces between rows

--- java_proiect5/src/Student.java
+++ java_proiect5/src/Student.java
@@ -1,8 +1,8 @@
 public class Student extends Persoana {
 private int anStudiu;
 
-public Student (String nume, String adresa, Facultate facultate, int anStudiu){
-    super(nume, adresa, facultate);
+public Student (String nume, String adresa, Facultate facultate, int anStudiu){   
+     super(nume, adresa, facultate);
     this.anStudiu=anStudiu;
 }
 public String toString(){
